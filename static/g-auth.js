8d91455c7c76490a7ca0a8cc130b83ac606d2c52
vanshikasahgal <64560283+vanshikasahgal@users.noreply.github.com>
2021-08-30 20:47:08 +0530

    Added google functionality

--- static/g-auth.js
+++ static/g-auth.js
@@ -1,1 +1,18 @@
+function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+}
 
+// To show google profile details on dashboard, use the following commands
+function getUserInfo(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  document.querySelector('#content').innerText=profile.getName();
+  document.querySelector('#content').innerText=profile.getEmail();
+}
+
+//To sign out, implement a sign out button on dashboard with the following function
+function signOut() {
+    gapi.auth2.getAuthInstance().signOut().then(function() {
+    console.log('User has signed out')
+  })
+}
+  
